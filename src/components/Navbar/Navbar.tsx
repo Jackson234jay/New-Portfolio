03c8afb4f9d9c4e98885ff285e0708741f3463c8
Jackson234jay <blossomjohn399@gmail.com>
2025-05-01 19:01:42 +0100

    Nav background

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "../ThemeToggle/ThemeToggle";
 const Navbar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar  shadow-sm">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
