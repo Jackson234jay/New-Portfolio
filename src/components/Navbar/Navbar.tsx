7eb44ba11621fde22053114eb18990d76a1d73d2
Jackson234jay <blossomjohn399@gmail.com>
2025-05-03 19:48:46 +0100

    Nav fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -29,21 +29,19 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a>Item 1</a>
+                <Link to="about" smooth={true}>
+                  About
+                </Link>
               </li>
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
+                <Link to="skills" smooth={true}>
+                  Skills
+                </Link>
               </li>
               <li>
-                <a>Item 3</a>
+                <Link to="contact" smooth={true}>
+                  Contact
+                </Link>
               </li>
             </ul>
           </div>
