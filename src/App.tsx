0f16d6efc645c1a8d254fc5ac93174aca4ede975
Jackson234jay <blossomjohn399@gmail.com>
2025-05-03 20:22:57 +0100

    Lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Hero from "./components/Hero";
-import Lightning from "./components/Lightning";
+import { Suspense, lazy } from "react";
 import { GlowCapture } from "@codaworks/react-glow";
 import Navbar from "./components/Navbar/Navbar";
-import Skills from "./components/Skills";
-import AudioPlayer from "./components/AudioPlayer";
+import Hero from "./components/Hero";
+import Lightning from "./components/Lightning";
 import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
+  const About = lazy(() => import("./components/About"));
+  const Contact = lazy(() => import("./components/Contact"));
+  const Skills = lazy(() => import("./components/Skills"));
+  const AudioPlayer = lazy(() => import("./components/AudioPlayer"));
   return (
     <div id="home" className="relative bg-base-200 overflow-hidden">
-      {/* Lightning background layer */}
       <div className="absolute inset-0 z-0">
         <Lightning />
         <Analytics />
       </div>
 
-      {/* Main content */}
       <div className="relative z-10">
         <Navbar />
-        <AudioPlayer />
+        <Suspense fallback={<div>Loading audio...</div>}>
+          <AudioPlayer />
+        </Suspense>
         <Hero />
-        <About />
-        <Skills />
-        <GlowCapture>
-          <Contact />
-        </GlowCapture>
+        <Suspense fallback={<div>Loading about...</div>}>
+          <About />
+        </Suspense>
+        <Suspense fallback={<div>Loading skills...</div>}>
+          <Skills />
+        </Suspense>
+        <Suspense fallback={<div>Loading contact...</div>}>
+          <GlowCapture>
+            <Contact />
+          </GlowCapture>
+        </Suspense>
       </div>
     </div>
   );
